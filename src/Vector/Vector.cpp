03fdde851ff422061039856d7a408b35fca7795c
Pang_GJ <2362421192@qq.com>
2022-01-29 11:34:35 +0800

    Change std::max() to my own max()

--- src/Vector/Vector.cpp
+++ src/Vector/Vector.cpp
@@ -1,13 +1,16 @@
 #include "Vector.h"
 
-#include <algorithm>
-
 #define DEFAULT_CAPACITY 3  // 默认初始容量
+
+template <typename T>
+T max(T t1, T t2) {
+  return t1 > t2 ? t1 : t2;
+}
 
 // 以数组区间 A[low, high) 为蓝本复制向量
 template <typename T>
 void Vector<T>::copyFrom(T const *A, Rank low, Rank high) {
-  _elem = new T[_capacity = std::max(2 * (high - low), DEFAULT_CAPACITY)];
+  _elem = new T[_capacity = max(2 * (high - low), DEFAULT_CAPACITY)];
   _size = 0;
   while (low < high) {
     _elem[_size++] = A[low++];
@@ -19,7 +22,7 @@ template <typename T>
 void Vector<T>::expand() {
   if (_size < _capacity) return;
 
-  _capacity = std::max(_capacity, DEFAULT_CAPACITY);
+  _capacity = max(_capacity, DEFAULT_CAPACITY);
   T *_oldElem = _elem;
   _elem = new T[_capacity <<= 1];  // 左移 1 位，相当于 2倍增
   for (int i = 0; i < _size; ++i) _elem[i] = _oldElem[i];
