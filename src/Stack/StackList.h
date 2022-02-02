937a20728f4143f9ab6adfcc56786f338a18a049
Pang_GJ <2362421192@qq.com>
2022-02-02 20:52:28 +0800

    Add Stack

---
+++ src/Stack/StackList.h
@@ -0,0 +1,22 @@
+#ifndef STACKLIST_H_
+#define STACKLIST_H_
+
+#include "../List/List.h"
+
+template <typename T>
+class Stack : public List<T> {
+  // push pop top
+  T top() const {
+    return this->last()->data;
+  }
+
+  T pop() {
+    return remove(this->last());
+  }
+
+  void push(const T &e) {
+    this->insertAsLast(e);
+  }
+};
+
+#endif
---
+++ src/Stack/StackVector.h
@@ -0,0 +1,24 @@
+#ifndef STACKVECTOR_H_
+#define STACKVECTOR_H_
+
+#include "../Vector/Vector.h"
+
+template <typename T>
+class Stack : public Vector<T> {
+ public:
+  // push pop top
+  Stack() = default;
+  T pop() {
+    return remove(this->size() - 1);
+  }
+
+  T top() const {
+    return (*this)[this->size() - 1];
+  } 
+
+  void push(const T &e) {
+    insert(this->size(), e);
+  }
+};
+
+#endif
