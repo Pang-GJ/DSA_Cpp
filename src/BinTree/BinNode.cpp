7b9404860699b91b27114734c12ae4c7f77f2ac1
Pang_GJ <2362421192@qq.com>
2022-02-18 11:36:31 +0800

    Add travelPre

--- src/BinTree/BinNode.cpp
+++ src/BinTree/BinNode.cpp
@@ -1,13 +1,38 @@
 #include "BinNode.h"
 
-template<typename T>
+#include <cstdlib>
+
+template <typename T>
 void BinNode<T>::insertAtLeft(const T &e) {
-  if (!_lChild)
-    _lChild = new BinNode<T>(e, this);
+  if (!_lChild) _lChild = new BinNode<T>(e, this);
 }
 
-template<typename T>
+template <typename T>
 void BinNode<T>::insertAtRight(const T &e) {
-  if (!_rChild)
-    _rChild = new BinNode<T>(e, this);
+  if (!_rChild) _rChild = new BinNode<T>(e, this);
 }
+
+template <typename T>
+int BinNode<T>::size() {
+  int sz = 1;
+  if (_lChild != nullptr) sz += _lChild->size();
+  if (_rChild != nullptr) sz += _rChild->size();
+  return sz;
+}
+
+template <typename T>
+template <typename VST>
+void BinNode<T>::travPre(VST &visit) {
+  switch (rand() % 3) {
+    case 1:
+      travPre_I1(this, visit);
+      break;
+    case 2:
+      travPre_I2(this, visit);
+      break;
+    default:
+      travPre_R(this, visit);
+      break;
+  }
+}
+
--- src/BinTree/BinNode.h
+++ src/BinTree/BinNode.h
@@ -2,6 +2,11 @@
 #define BINNODE_H_
 
 template <typename T>
+struct BinNode;
+template <typename T>
+using BinNodePtr = BinNode<T> *;
+
+template <typename T>
 struct BinNode {
   // parent ,leftChild, rightChild, height, data
   BinNode<T> *_parent;
@@ -9,15 +14,36 @@ struct BinNode {
   BinNode<T> *_rChild;
 
   T _data;
+  int _height;
 
   BinNode(int data = 0, BinNode<T> *parent = nullptr)
-      : _data(data), _parent(parent), _lChild(nullptr), _rChild(nullptr) {}
+      : _data(data),
+        _height(0),
+        _parent(parent),
+        _lChild(nullptr),
+        _rChild(nullptr) {}
 
   // 作为左节点插入
   void insertAtLeft(const T &e);
 
   // 作为右节点插入
   void insertAtRight(const T &e);
+
+  BinNodePtr<T> succ();  // 取出当前节点的直接后继
+  int size();            // 统计当前节点的后代总数
+
+  // 遍历
+  template <typename VST>
+  void travLevel(VST &);  // 层次遍历
+
+  template <typename VST>
+  void travPre(VST &);  // 先序遍历
+
+  template <typename VST>
+  void travIn(VST &);  // 中序遍历
+
+  template <typename VST>
+  void travPost(VST &);  // 后序遍历
 };
 
 #endif  // BINNODE_H_
